ecc476a354498ac159242308876d5b623ed77ad5
Khaled Oweas <khaled.oweas@yahoo.com>
2023-12-11 12:53:37 +0200

    ConsoleApp
    description: This is  a Test App

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(@"
+ _______    _    _     _____  
+(_______)  | |  / )   / ___ \ 
+ _____     | | / /   | |   | |
+|  ___)    | |< <    | |   | |
+| |_____   | | \ \   | |___| |
+|_______)  |_|  \_)   \_____/ 
+                              
+
+");
